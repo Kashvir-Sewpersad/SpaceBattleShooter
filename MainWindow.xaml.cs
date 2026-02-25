5177b7b79d3dd66ca5581241c0e63378fe949bb6
kashc <kashc@THE_BEAST>
2026-02-25 21:14:52 +0200

    got a basic UI to appear on screen upon build and run

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -46,6 +46,28 @@ namespace SpaceBattleShooter
         {
             InitializeComponent();
 
+
+
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            gameTimer.Tick += GameLoop;
+            gameTimer.Start();
+
+
+
+            MyCanvas.Focus();
+
+            ImageBrush bg = new ImageBrush();
+            bg.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/purple.png"));
+            bg.TileMode = TileMode.Tile;
+            bg.Viewport = new Rect(0, 0, 0.15, 0.15);
+            bg.ViewportUnits = BrushMappingMode.RelativeToBoundingBox;
+            MyCanvas.Background = bg;
+
+        }
+
+        private void GameLoop(object sender, EventArgs e)
+        {
+            
         }
 
         private void OnKeyDown(object sender, KeyEventArgs e)
